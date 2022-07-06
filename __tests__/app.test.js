97b8697f85e09e796d92a593569ef14ef1f43ad2
MRombi <michael.rombi@hotmail.com>
2022-07-06 09:08:39 +0100

    add endpoint /api/reviews with its functionality and error handling implemented with TDD

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -55,7 +55,7 @@ describe("/api/reviews/review_id", () => {
           expect(body.review.title).toBe("Jenga");
           expect(body.review.designer).toBe("Leslie Scott");
           expect(body.review.owner).toBe("philippaclaire9");
-          expect(body.review.comment_count).toBe("3")
+          expect(body.review.comment_count).toBe("3");
         });
     });
   });
@@ -153,6 +153,40 @@ describe("GET: /api/users", () => {
       });
   });
   describe("ERRORS - GET: /api/users", () => {
+    test("404: bad path if wrong endpoint written", () => {
+      return request(app)
+        .get("/api/testing")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.message).toBe("Path not found");
+        });
+    });
+  });
+});
+
+describe("GET: /api/reviews", () => {
+  test("200:  returns an array of review objects, each of which should have the properties of owner, title review_id, category, review_img_url, created_at, votes, review_body, designer, comment_count", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.reviews[0]).toEqual({
+          review_id: 7,
+          title: "Mollit elit qui incididunt veniam occaecat cupidatat",
+          category: "social deduction",
+          designer: "Avery Wunzboogerz",
+          owner: "mallionaire",
+          review_body:
+            "Consectetur incididunt aliquip sunt officia. Magna ex nulla consectetur laboris incididunt ea non qui. Enim id eiusmod irure dolor ipsum in tempor consequat amet ullamco. Occaecat fugiat sint fugiat mollit consequat pariatur consequat non exercitation dolore. Labore occaecat in magna commodo anim enim eiusmod eu pariatur ad duis magna. Voluptate ad et dolore ullamco anim sunt do. Qui exercitation tempor in in minim ullamco fugiat ipsum. Duis irure voluptate cupidatat do id mollit veniam culpa. Velit deserunt exercitation amet laborum nostrud dolore in occaecat minim amet nostrud sunt in. Veniam ut aliqua incididunt commodo sint in anim duis id commodo voluptate sit quis.",
+          review_img_url:
+            "https://images.pexels.com/photos/278888/pexels-photo-278888.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          created_at: "2021-01-25T11:16:54.963Z",
+          votes: 9,
+          comment_count: "0",
+        });
+      });
+  });
+  describe("ERRORS - GET: /api/reviews", () => {
     test("404: bad path if wrong endpoint written", () => {
       return request(app)
         .get("/api/testing")
